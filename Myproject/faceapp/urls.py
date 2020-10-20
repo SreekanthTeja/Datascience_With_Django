8fae44e5e0ac180e17bccbf64cdbf2412d0c24e6
SreekanthTeja <62073040+SreekanthTeja@users.noreply.github.com>
2020-10-20 16:56:44 +0530

    Update urls.py

--- Myproject/faceapp/urls.py
+++ Myproject/faceapp/urls.py
@@ -2,7 +2,6 @@ from django.urls import path
 from .views import imageviewform
 
 urlpatterns=[
-    # path('face/',facerecognition,name='face'),
     path('image/',imageviewform,name='image'),
     
 ]
